fb8aba0f5f71838edfc1f9cb0956a57afc9c6949
Alan Ades <alan.ades2@gmail.com>
2024-10-29 19:43:47 -0300

    Busqueda de solicitudes finalizado

--- src/app/componentes/table/TableBodyRow.jsx
+++ src/app/componentes/table/TableBodyRow.jsx
@@ -1,12 +1,16 @@
 import Acciones from "./Acciones";
 
+const getNestedValue = (obj, path) => {
+  return path.split('.').reduce((acc, part) => acc && acc[part], obj) || "S/D";
+};
+
 export default function TableBodyRow(props) {
   const { dato, keys, acciones } = props;
   return (
     <tr key={dato._id}>
       {keys.map((key) => (
         <td key={key} className="whitespace-nowrap px-4 py-2 text-center">
-          {dato[key] || "S/D"}
+          {getNestedValue(dato, key)}
         </td>
       ))}
       {acciones && (
