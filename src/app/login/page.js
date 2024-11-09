df64bb2757fbdae81ac0843350ad87007879c115
lukitasmois <lmoix85@gmail.com>
2024-11-09 15:16:04 -0300

    Agrego pagina para cambiar la contraseña

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -77,11 +77,11 @@ export default function PageLogin() {
 
   return (
     <div className="flex justify-center items-center min-h-screen bg-gray-100">
-      <div className="mx-auto my-36 flex h-[350px] w-[400px] flex-col border-2 bg-white text-black shadow-xl">
+      <div className="mx-auto my-36 flex h-auto w-[400px] flex-col border-2 bg-white text-black shadow-xl">
         <div className="mx-8 mt-7 mb-3 flex flex-row justify-start space-x-2">
           <div className="h-7 w-3 bg-[#0DE6AC]"></div>
-          <div className="w-3 text-center font-sans text-xl font-bold">
-            <h1>Login</h1>
+          <div className="text-center font-sans text-xl font-bold">
+            <h1>Inicio de sesión</h1>
           </div>
         </div>
         <form onSubmit={handleSubmit} className="flex flex-col items-center">
@@ -107,7 +107,7 @@ export default function PageLogin() {
           <div className="my-2 flex justify-center">
             <button className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
               <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-                Iniciar sesion
+                Iniciar sesión
               </span>
             </button>
           </div>
@@ -116,10 +116,10 @@ export default function PageLogin() {
           <small className="text-red-500 text-xs italic mt-1 text-center">
             {errors.submit}
           </small>
-        )}{" "}
+        )}
         <div className="mx-7 my-3 flex justify-end text-sm font-semibold">
           <a href="/crearAsegurador" className="text-blue-500">
-            No tienes cuenta? Registrate aquí.
+            No tienes cuenta? Regístrate aquí.
           </a>
         </div>
       </div>
