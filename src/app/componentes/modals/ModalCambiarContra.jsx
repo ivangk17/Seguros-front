1d81d5f0c6ce7f3163c076c30ab7f894303562da
rodolfoconci <r.conci@hotmail.com>
2024-11-30 15:52:54 -0300

    cambio en modal de cambiar contraseña

--- src/app/componentes/modals/ModalCambiarContra.jsx
+++ src/app/componentes/modals/ModalCambiarContra.jsx
@@ -74,19 +74,19 @@ const ChangePasswordModal = ({ show, onClose, onSubmit }) => {
 
   return (
     <motion.div
-    className="fixed inset-0 flex items-center justify-center bg-gray-900 bg-opacity-50 z-50" // Añadimos z-50 aquí
-    initial={{ opacity: 0 }}
-    animate={{ opacity: 1 }}
-    exit={{ opacity: 0 }}
-    transition={{ duration: 0.3 }}
-  >
-    <motion.div
-      className="bg-white p-6 rounded-lg shadow-lg"
-      initial={{ scale: 0.9 }}
-      animate={{ scale: 1 }}
-      exit={{ scale: 0.9 }}
+      className="fixed inset-0 flex items-center justify-center bg-gray-900 bg-opacity-50 z-50" // Añadimos z-50 aquí
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
       transition={{ duration: 0.3 }}
     >
+      <motion.div
+        className="bg-white p-6 rounded-lg shadow-lg"
+        initial={{ scale: 0.9 }}
+        animate={{ scale: 1 }}
+        exit={{ scale: 0.9 }}
+        transition={{ duration: 0.3 }}
+      >
         <div className="flex justify-between items-center mb-4">
           <h2 className="text-lg font-semibold text-black dark:text-white">
             Cambiar Contraseña
@@ -99,30 +99,36 @@ const ChangePasswordModal = ({ show, onClose, onSubmit }) => {
           </button>
         </div>
         <form onSubmit={handleSubmit}>
-          <Input
-            name="oldPass"
-            label="Contraseña Actual"
-            type="password"
-            value={formData.oldPass}
-            onChange={handleInputChange}
-            error={errors.oldPass}
-          />
-          <Input
-            name="newPass"
-            label="Nueva Contraseña"
-            type="password"
-            value={formData.newPass}
-            onChange={handleInputChange}
-            error={errors.newPass}
-          />
-          <Input
-            name="confirmPassword"
-            label="Confirmar Nueva Contraseña"
-            type="password"
-            value={formData.confirmPassword}
-            onChange={handleInputChange}
-            error={errors.confirmPassword}
-          />
+          <div className="mb-2">
+            <Input
+              name="oldPass"
+              label="Contraseña Actual"
+              type="password"
+              value={formData.oldPass}
+              onChange={handleInputChange}
+              error={errors.oldPass}
+            />
+          </div>
+          <div className="mb-2">
+            <Input
+              name="newPass"
+              label="Nueva Contraseña"
+              type="password"
+              value={formData.newPass}
+              onChange={handleInputChange}
+              error={errors.newPass}
+            />
+          </div>
+          <div className="mb-2">
+            <Input
+              name="confirmPassword"
+              label="Confirmar Nueva Contraseña"
+              type="password"
+              value={formData.confirmPassword}
+              onChange={handleInputChange}
+              error={errors.confirmPassword}
+            />
+          </div>
           <div className="mt-4 flex justify-end">
             <button
               type="button"
